681be68cc4584061958409d6bc4039f7211eec62
Eric-Noblot <eric.noblot@gmail.com>
2024-01-30 15:45:54 +0100

    query selectors & eventslisteners

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
+// GESTION DES INPUT
 
+const departure = document.querySelector("#inputDeparture")
+const arrival = document.querySelector("#inputArrival")
+const date = document.querySelector("#inputDate")
 
+departure.addEventListener("keydown", () => {
+    console.log(departure.value)
+})
+
+//GESTION DU BOUTON
+const button = document.querySelector("#search_btn")
+
+button.addEventListener("click", () => {
+    alert(departure.value)
+})
